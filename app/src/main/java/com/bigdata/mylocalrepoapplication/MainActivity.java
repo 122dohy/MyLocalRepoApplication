67a55cc3c414d44f67eecf38b905ea0183b35b66
122dohy <122dohy@gmail.com>
2017-06-19 14:32:14 +0900

    Initial commit

---
+++ app/src/main/java/com/bigdata/mylocalrepoapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bigdata.mylocalrepoapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*
+    This is a main activity.
+    */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
